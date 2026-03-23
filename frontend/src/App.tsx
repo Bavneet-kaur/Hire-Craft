985d80ada6398ff3ccf25d6a28d9fc45ee59644b
Bavneet Kaur <bavneetgulab@gmail.com>
2026-03-23 17:53:06 +0530

    created the loader

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,18 @@
 // import React from 'react'
 import './index.scss';
 import { RouterProvider } from 'react-router';
-import {router} from "./routes/app.routes"
+import { router } from "./routes/app.routes"
+// import Loader from './components/loader';
 function App() {
 
   return (
     <>
-    <div>
-      <RouterProvider router = {router} />
-    </div>
+      <div>
+        <RouterProvider router={router} />
+        {/* <div style={{ display: "flex", justifyContent: "center", marginTop: "100px", height:"screen" }}>
+          <Loader />
+        </div> */}
+      </div>
     </>
   )
 }
