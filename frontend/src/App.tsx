72102b1d0e1786d3a081ffc60e5a35b266675bc8
Bavneet Kaur <bavneetgulab@gmail.com>
2026-03-23 20:20:49 +0530

    created the API layer- using react query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,16 +3,16 @@
 import './index.scss';
 import { RouterProvider } from 'react-router';
 import { router } from "./routes/app.routes"
-// import Loader from './components/loader';
+import Loader from './components/loader';
 function App() {
 
   return (
     <>
       <div>
         <RouterProvider router={router} />
-        {/* <div style={{ display: "flex", justifyContent: "center", marginTop: "100px", height:"screen" }}>
+        <div style={{ display: "flex", justifyContent: "center", marginTop: "100px", height:"screen" }}>
           <Loader />
-        </div> */}
+        </div>
       </div>
     </>
   )
