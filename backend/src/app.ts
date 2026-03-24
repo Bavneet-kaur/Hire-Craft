44e318dd7528125d8e2c4706f4744818b24de961
Bavneet Kaur <bavneetgulab@gmail.com>
2026-03-24 20:18:51 +0530

    resolved the CORS error while login

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,18 @@ import express from "express";
 import connectDB from "./config/db";
 import authRouter from "./routes/auth";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 connectDB();
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+    })
+);
 app.use("/api/auth", authRouter);
 export default app;
