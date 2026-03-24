c25f41f0d516f5b26083ccba50a7156df164f6f3
Bavneet Kaur <bavneetgulab@gmail.com>
2026-03-24 15:43:28 +0530

    implemented the hook layer under auth

---
+++ frontend/src/features/Authentication/useAuth.ts
@@ -0,0 +1,73 @@
+import { useContext } from "react";
+import { AuthContext } from "./auth.context";
+import { loginUser, registerUser, logoutUser, getMe } from "./auth.api";
+
+interface Input {
+    name: string;
+    email: string;
+    password: string;
+}
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) {
+        throw new Error("useAuth must be used inside AuthProvider");
+    }
+    const { user, setUser, loading, setLoading } = context;
+    const handleRegister = async ({ name, email, password }: Input) => {
+        try {
+            setLoading(true);
+            const data = await registerUser({ name, email, password });
+            setUser(data.user);
+            return data;
+        } catch (error: any) {
+            throw new Error(error.message || "Login failed");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleLogin = async ({ email, password }: Input) => {
+        try {
+            setLoading(true);
+            const data = await loginUser({ email, password });
+            setUser(data.user);
+            return data;
+        } catch (error: any) {
+            throw new Error(error.message || "Login failed");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const handleLogout = async () => {
+        try {
+            setLoading(true);
+            await logoutUser();
+            setUser(null);
+        } catch (error: any) {
+            throw new Error(error.message || "Logout failed");
+        } finally {
+            setLoading(false);
+        }
+    };
+    const fetchMe = async () => {
+        try {
+            setLoading(true);
+            const data = await getMe();
+            setUser(data.user);
+        } catch (error) {
+            setUser(null);
+        } finally {
+            setLoading(false);
+        }
+    };
+    return {
+        user,
+        loading,
+        handleRegister,
+        handleLogin,
+        handleLogout,
+        fetchMe,
+    };
+}
