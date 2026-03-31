5f817ee2ca4abde505d31a2b7b08c5ad826113f2
Bavneet Kaur <bavneetgulab@gmail.com>
2026-04-01 03:00:24 +0530

    updated the app and frontend routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,16 @@ import './index.scss';
 import { RouterProvider } from 'react-router';
 import { router } from "./routes/app.routes"
 import { AuthProvider } from './features/Authentication/auth.context';
+import { ReportProvider } from './features/AI/report.context';
 
 function App() {
 
   return (
     <>
       <AuthProvider>
-        <RouterProvider router={router} />
+        <ReportProvider>
+          <RouterProvider router={router} />
+        </ReportProvider>
       </AuthProvider>
     </>
   )
